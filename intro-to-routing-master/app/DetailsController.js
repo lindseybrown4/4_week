d6c92e6da686d9830c793b709e549d2343de35f8
lindseybrown4 <lindseybrown4@gmail.com>
2015-07-27 13:42:21 -0600

    initial commit

---
+++ intro-to-routing-master/app/DetailsController.js
@@ -0,0 +1,8 @@
+app.controller('DetailsController', function($scope, DataService, $routeParams){
+
+var peopleIndex = $routeParams.peopleId; 
+
+$scope.name = DataService.data[peopleIndex].name; 
+$scope.description = DataService.data[peopleIndex].description;  
+
+});
